f8db19c3c8ae3a7a2df35515afd60f0a9eeb5cdf
Sylvain Blondeau <sylvain.blondeau@wildcodeschool.fr>
2023-06-07 17:27:31 +0200

    add guide

--- src/Entity/Guide.php
+++ src/Entity/Guide.php
@@ -2,12 +2,14 @@
 
 namespace App\Entity;
 
-use App\Repository\GuideRepository;
 use DateTime;
+use ORM\NotBlank;
 use DateTimeImmutable;
 use DateTimeInterface;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use App\Repository\GuideRepository;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: GuideRepository::class)]
 class Guide
@@ -18,9 +20,12 @@ class Guide
     private ?int $id = null;
 
     #[ORM\Column(length: 255)]
+    #[Assert\NotBlank]
+    #[Assert\Length(max: 255)]
     private ?string $title = null;
 
     #[ORM\Column(type: Types::TEXT)]
+    #[Assert\NotBlank]
     private ?string $content = null;
 
     #[ORM\Column(length: 255, nullable: true)]
--- src/Form/GuideType.php
+++ src/Form/GuideType.php
@@ -4,6 +4,8 @@ namespace App\Form;
 
 use App\Entity\Guide;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,10 +14,16 @@ class GuideType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('title')
-            ->add('content')
+            ->add('title', TextType::class, [
+                'label' => 'Titre',
+                'attr' => [
+                    'placeholder' => 'Mon guide de l\'immobilier',
+                ]
+            ])
+            ->add('content', TextareaType::class, [
+                'label' => 'Contenu',
+            ])
             ->add('image')
-            ->add('createdAt')
         ;
     }
 
