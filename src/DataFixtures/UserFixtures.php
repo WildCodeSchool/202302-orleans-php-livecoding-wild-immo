54bda0bdb2120a978499ac69ed02dbdc3763375a
Sylvain Blondeau <sylvain.blondeau@wildcodeschool.fr>
2023-06-13 14:59:05 +0200

    login

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class UserFixtures extends Fixture
+{
+    public function __construct(private UserPasswordHasherInterface $userPasswordHasher)
+    {
+    }
+
+    public function load(ObjectManager $manager): void
+    {
+        $admin = new User();
+        $admin->setEmail('admin@wildimmo.com');
+        $admin->setPassword($this->userPasswordHasher->hashPassword($admin, 'azerty'));
+        $manager->persist($admin);
+        $manager->flush();
+    }
+}
