e6efd3be9389f1ca9b80f778decbc799f3c6689f
Sylvain Blondeau <sylvain.blondeau@wildcodeschool.fr>
2023-06-21 11:10:06 +0200

    admin new + fix style

--- src/Form/EstateType.php
+++ src/Form/EstateType.php
@@ -6,7 +6,10 @@ use App\Entity\Estate;
 use Faker\Core\Number;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\MoneyType;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Vich\UploaderBundle\Form\Type\VichFileType;
@@ -16,14 +19,27 @@ class EstateType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('title')
-            ->add('price', NumberType::class)
-            ->add('surface', NumberType::class)
-            ->add('address')
-            ->add('city')
-            ->add('description')
-            ->add('imageFile', VichFileType::class)
-        ;
+            ->add('title', TextType::class, [
+                'label' => 'Titre de l\'annonce',
+                'attr' => [
+                    'placeholder' => 'Maison de ville',
+                ]
+            ])
+            ->add('price', MoneyType::class, [
+                'label' => 'Prix',
+            ])
+            ->add('surface', NumberType::class, [
+                'help' => 'En m<sup>2</sup>',
+                'help_html' => true,
+            ])
+            ->add('address', TextType::class, [
+                'label' => 'Adresse',
+            ])
+            ->add('city', TextType::class, [
+                'label' => 'Ville'
+            ])
+            ->add('description', TextareaType::class)
+            ->add('imageFile', VichFileType::class);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
