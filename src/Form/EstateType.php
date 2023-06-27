72cb43f7da5ad9dc28cdcc41d2c3cb102bedb1b5
Sylvain Blondeau <sylvain.blondeau@wildcodeschool.fr>
2023-06-27 14:32:54 +0200

    form entity type category

--- src/Form/EstateType.php
+++ src/Form/EstateType.php
@@ -4,21 +4,33 @@ namespace App\Form;
 
 use App\Entity\Estate;
 use Faker\Core\Number;
+use App\Entity\EstateCategory;
+use Doctrine\ORM\QueryBuilder;
+use Doctrine\ORM\EntityRepository;
 use Symfony\Component\Form\AbstractType;
+use Vich\UploaderBundle\Form\Type\VichFileType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\MoneyType;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\OptionsResolver\OptionsResolver;
-use Vich\UploaderBundle\Form\Type\VichFileType;
 
 class EstateType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
+            ->add('estateCategory', EntityType::class, [
+                'class' => EstateCategory::class,
+                'choice_label' => 'name',
+                'query_builder' => function (EntityRepository $entityRepository): QueryBuilder {
+                    return $entityRepository->createQueryBuilder('ec')
+                        ->orderBy('ec.name', 'ASC');
+                },
+            ])
             ->add('title', TextType::class, [
                 'label' => 'Titre de l\'annonce',
                 'attr' => [
