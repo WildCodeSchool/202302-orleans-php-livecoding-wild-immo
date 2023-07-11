4afe65b9c74958775d92997fd3b4e316b9b16a9f
Sylvain Blondeau <sylvain.blondeau@wildcodeschool.fr>
2023-07-11 14:59:35 +0200

    display collection type

--- src/Form/EstateType.php
+++ src/Form/EstateType.php
@@ -4,9 +4,11 @@ namespace App\Form;
 
 use App\Entity\Estate;
 use Faker\Core\Number;
+use App\Entity\Caracteristic;
 use App\Entity\EstateCategory;
 use Doctrine\ORM\QueryBuilder;
 use Doctrine\ORM\EntityRepository;
+use App\Form\EstateCaracteristicType;
 use Symfony\Component\Form\AbstractType;
 use Vich\UploaderBundle\Form\Type\VichFileType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -17,6 +19,7 @@ use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\MoneyType;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 
 class EstateType extends AbstractType
 {
@@ -53,6 +56,9 @@ class EstateType extends AbstractType
             ->add('description', TextareaType::class)
             ->add('imageFile', VichFileType::class, [
                 'required' => false,
+            ])
+            ->add('estateCaracteristics', CollectionType::class, [
+                'entry_type' => EstateCaracteristicType::class
             ]);
     }
 
